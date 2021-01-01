21c88e44cf4fdad85f618ffa0cd0c7747db68782
JiD-pro <75407611+JiD-pro@users.noreply.github.com>
2021-01-01 16:55:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import FirstContainer from './Components/FirstPage/FirstContainer/FirstContainer.jsx';
+import SecondContainer from './Components/FirstPage/SecondContainer/SecondContainer.jsx';
+import ThirdContainer from './Components/FirstPage/ThirdContainer/ThirdContainer.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FourthContainer from './Components/FirstPage/FourthContainer/FourthContainer.jsx';
+
+const App = () => {
+  return(
+    <div>
+      <FirstContainer />
+      <SecondContainer />
+      <ThirdContainer />
+      <FourthContainer />
+    </div>
+  );
+}
+
+export default App;
